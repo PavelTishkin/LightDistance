aba95fde9fe336180622e3d985576f9dba2d29bd
Pavlo Tishkin <pavlo.tishkin@gmail.com>
2016-04-11 22:09:50 +0100

    Set background color based on user input

--- app/src/main/java/pavlo/com/lightdistance/LightDistanceActivity.java
+++ app/src/main/java/pavlo/com/lightdistance/LightDistanceActivity.java
@@ -1,7 +1,9 @@
 package pavlo.com.lightdistance;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LightDistanceActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class LightDistanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_distance);
+    }
+
+    public void onRedButtonClick(View view) {
+        getWindow().getDecorView().setBackgroundColor(Color.argb(255, 255, 0, 0));
+    }
+
+    public void onGreenButtonClick(View view) {
+        getWindow().getDecorView().setBackgroundColor(Color.argb(255, 0, 255, 0));
+    }
+
+    public void onBlueButtonClick(View view) {
+        getWindow().getDecorView().setBackgroundColor(Color.argb(255, 0, 0, 255));
     }
 }
